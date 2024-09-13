ef1922dc434d32a0d6015827fb30db6d9928f39f
Juanadelacuesta <8647634+Juanadelacuesta@users.noreply.github.com>
2024-09-13 17:07:04 +0200

    test: add tests to driver

--- virt/driver_test.go
+++ virt/driver_test.go
@@ -5,11 +5,14 @@ package virt
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
 	"testing"
 	"time"
 
 	domain "github.com/hashicorp/nomad-driver-virt/internal/shared"
+	"github.com/hashicorp/nomad-driver-virt/libvirt"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/ci"
@@ -22,27 +25,43 @@ import (
 	"github.com/shoenig/test/must"
 )
 
-type mockImageHandler struct{}
+type mockImageHandler struct {
+	basePath    string
+	imageFormat string
+	err         error
+}
 
 func (mh *mockImageHandler) GetImageFormat(basePath string) (string, error) {
-	return "", nil
+	mh.basePath = basePath
+	return mh.imageFormat, mh.err
 }
 
 func (mh *mockImageHandler) CreateThinCopy(basePath string, destination string, sizeM int64) error {
 	return nil
 }
 
-type mockTaskGetter struct{}
+type mockTaskGetter struct {
+	count int
+	info  *domain.Info
+	err   error
+}
 
 func (mtg *mockTaskGetter) GetDomain(name string) (*domain.Info, error) {
-	return nil, nil
+	mtg.count += 1
+	return mtg.info, mtg.err
 }
 
 type mockVirtualizar struct {
+	config *domain.Config
+	count  int
+	err    error
 }
 
 func (mv *mockVirtualizar) CreateDomain(config *domain.Config) error {
-	return nil
+	mv.count += 1
+	mv.config = config
+
+	return mv.err
 }
 
 func (mv *mockVirtualizar) StopDomain(name string) error {
@@ -50,6 +69,7 @@ func (mv *mockVirtualizar) StopDomain(name string) error {
 }
 
 func (mv *mockVirtualizar) DestroyDomain(name string) error {
+	mv.count -= 1
 	return nil
 }
 
@@ -61,7 +81,7 @@ func createBasicResources() *drivers.Resources {
 	res := drivers.Resources{
 		NomadResources: &structs.AllocatedTaskResources{
 			Memory: structs.AllocatedMemoryResources{
-				MemoryMB: 100,
+				MemoryMB: 6000,
 			},
 			Cpu: structs.AllocatedCpuResources{
 				CpuShares: 250,
@@ -100,6 +120,9 @@ func virtDriverHarness(t *testing.T, v Virtualizer, cfg map[string]interface{},
 
 	d := NewPlugin(logger).(*VirtDriverPlugin)
 	must.NoError(t, d.SetConfig(baseConfig))
+	d.virtualizer = v
+	d.imageHandler = ih
+	d.taskGetter = dg
 
 	d.buildFingerprint()
 
@@ -108,80 +131,208 @@ func virtDriverHarness(t *testing.T, v Virtualizer, cfg map[string]interface{},
 	return harness
 }
 
-/* func Test_StartAndStopTask(t *testing.T) {
-	logger := hclog.NewNullLogger()
-
-	mv := &mockVirtualizar{}
-	mtg := &mockTaskGetter{}
-	mih := &mockImageHandler{}
-
-	vp := &VirtDriverPlugin{
-		config: &Config{
-			ImagePaths: []string{"valid/path/here"},
-		},
-		tasks:        newTaskStore(),
-		logger:       logger,
-		virtualizer:  mv,
-		taskGetter:   mtg,
-		imageHandler: mih,
-	}
-
-	th, dn, err := vp.StartTask(&drivers.TaskConfig{
-		ID: "allocID/taskName/12345678",
-	})
-	fmt.Println(th, dn, err)
-} */
-
-func newTaskConfig(image string, command []string) TaskConfig {
+func newTaskConfig(image string) TaskConfig {
 	return TaskConfig{
 		ImagePath:           image,
 		UserData:            "/user/data/path",
-		CMDs:                command,
+		CMDs:                []string{"cmd arg arg", "cmd arg arg"},
 		DefaultUserSSHKey:   "ssh-ed666 randomkey",
 		DefaultUserPassword: "password",
-		UseThinCopy:         true,
-		PrimaryDiskSize:     666,
+		//UseThinCopy:         true,
+		PrimaryDiskSize: 2666,
+		OS: &OS{
+			Arch:    "arch",
+			Machine: "machine",
+		},
 	}
 }
 
-func TestVirtDriver_Start_Wait(t *testing.T) {
+func TestVirtDriver_StartTask_Wait_Destroy(t *testing.T) {
 	ci.Parallel(t)
 
-	tempFile, err := os.CreateTemp("", "testfile-*.txt")
+	tempDir, err := os.MkdirTemp("", "exampledir-*")
 	must.NoError(t, err)
-	defer os.Remove(tempFile.Name())
+	defer os.RemoveAll(tempDir)
 
-	taskCfg := newTaskConfig("", []string{"cmd arg arg", "cmd arg arg"})
+	mockImage, err := os.CreateTemp(tempDir, "test-*.img")
+	must.NoError(t, err)
+	defer os.Remove(mockImage.Name())
+
+	allocID := uuid.Generate()
+	taskCfg := newTaskConfig(mockImage.Name())
+
+	taskID := fmt.Sprintf("%s/%s/%s", allocID[:7], "task-name", "0000000")
 	task := &drivers.TaskConfig{
-		ID:        uuid.Generate(),
-		Name:      "start_wait",
-		AllocID:   uuid.Generate(),
+		ID:        taskID,
+		AllocID:   allocID,
 		Resources: createBasicResources(),
 	}
+
 	must.NoError(t, task.EncodeConcreteDriverConfig(&taskCfg))
 
-	mv := &mockVirtualizar{}
-	mtg := &mockTaskGetter{}
-	mih := &mockImageHandler{}
+	mockVirtualizer := &mockVirtualizar{
+		count: 0,
+	}
 
-	d := virtDriverHarness(t, mv, nil, mtg, mih, tempFile.Name())
+	mockTaskGetter := &mockTaskGetter{
+		count: 0,
+		info: &domain.Info{
+			State: libvirt.DomainRunning,
+		},
+	}
+
+	mockImageHandler := &mockImageHandler{
+		imageFormat: "tif",
+	}
+
+	d := virtDriverHarness(t, mockVirtualizer, nil, mockTaskGetter, mockImageHandler, tempDir)
 	cleanup := d.MkAllocDir(task, true)
 	defer cleanup()
 
-	_, _, err = d.StartTask(task)
+	dth, _, err := d.StartTask(task)
 	must.NoError(t, err)
 
-	defer func() {
-		_ = d.DestroyTask(task.ID, true)
-	}()
+	must.One(t, dth.Version)
+	must.One(t, mockVirtualizer.count)
+
+	// Assert the correct configuration was passed on to the virtualizer.
+	must.Eq(t, "task-name-0000000", mockVirtualizer.config.Name)
+	must.Eq(t, 6000, mockVirtualizer.config.Memory)
+	must.Eq(t, 250, mockVirtualizer.config.CPUs)
+	must.Eq(t, []string{"cmd arg arg", "cmd arg arg"}, mockVirtualizer.config.CMDs)
+	must.StrContains(t, "arch", mockVirtualizer.config.OsVariant.Arch)
+	must.StrContains(t, "machine", mockVirtualizer.config.OsVariant.Machine)
+	must.StrContains(t, mockImage.Name(), mockVirtualizer.config.BaseImage)
+	must.StrContains(t, "tif", mockVirtualizer.config.DiskFmt)
+	must.Eq(t, 2666, mockVirtualizer.config.PrimaryDiskSize)
+	must.StrContains(t, "nomad-task-name-0000000", mockVirtualizer.config.HostName)
+	must.Eq(t, 3, len(mockVirtualizer.config.Mounts))
+	must.Eq(t, domain.MountFileConfig{
+		Source:      task.AllocDir + "/alloc",
+		Destination: "/alloc",
+		ReadOnly:    true,
+		Tag:         "allocDir",
+	}, mockVirtualizer.config.Mounts[0])
+	must.Eq(t, domain.MountFileConfig{
+		Source:      task.AllocDir + "/local",
+		Destination: "/local",
+		ReadOnly:    true,
+		Tag:         "localDir",
+	}, mockVirtualizer.config.Mounts[1])
+	must.Eq(t, domain.MountFileConfig{
+		Source:      task.AllocDir + "/secrets",
+		Destination: "/secrets",
+		ReadOnly:    true,
+		Tag:         "secretsDir",
+	}, mockVirtualizer.config.Mounts[2])
+	must.StrContains(t, "ssh-ed666 randomkey", mockVirtualizer.config.SSHKey)
+	must.StrContains(t, "password", mockVirtualizer.config.Password)
+	must.Eq(t, []string{"cmd arg arg", "cmd arg arg"}, mockVirtualizer.config.CMDs)
+	must.Eq(t, []string{
+		"mkdir -p /alloc",
+		"mountpoint -q /alloc || mount -t 9p -o trans=virtio allocDir /alloc",
+		"mkdir -p /local",
+		"mountpoint -q /local || mount -t 9p -o trans=virtio localDir /local",
+		"mkdir -p /secrets",
+		"mountpoint -q /secrets || mount -t 9p -o trans=virtio secretsDir /secrets",
+	}, mockVirtualizer.config.BOOTCMDs)
 
 	// Attempt to wait
 	waitCh, err := d.WaitTask(context.Background(), task.ID)
 	must.NoError(t, err)
 
+	ctx, cancel := context.WithCancel(context.Background())
+
+	statsChan, err := d.TaskStats(ctx, task.ID, time.Second)
+	must.NoError(t, err)
+
+	go func(t *testing.T) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-statsChan:
+			case <-time.After(2 * time.Second):
+				t.Error("no stats comming from task")
+			}
+		}
+	}(t)
+
 	select {
 	case <-waitCh:
 		t.Fatalf("wait channel should not have received an exit result")
 	case <-time.After(10 * time.Second):
+	}
+
+	ts, err := d.InspectTask(task.ID)
+	must.NoError(t, err)
+
+	must.StrContains(t, task.ID, ts.ID)
+	// Assert the correct monitoring
+	must.NonZero(t, mockTaskGetter.count)
+
+	cancel()
+	err = d.DestroyTask(task.ID, true)
+	must.NoError(t, err)
+	must.Zero(t, mockVirtualizer.count)
+}
+
+func TestVirtDriver_StartTask(t *testing.T) {
+	ci.Parallel(t)
+
+	tempDir, err := os.MkdirTemp("", "exampledir-*")
+	must.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+
+	mockImage, err := os.CreateTemp(tempDir, "test-*.img")
+	must.NoError(t, err)
+	defer os.Remove(mockImage.Name())
+
+	testsCases := []struct {
+		name    string
+		expErr  error
+		virtErr error
+	}{
+		{},
+	}
+
+	for _, tt := range testsCases {
+		t.Run(tt.name, func(t *testing.T) {
+			allocID := uuid.Generate()
+			taskCfg := newTaskConfig(mockImage.Name())
+
+			taskID := fmt.Sprintf("%s/%s/%s", allocID[:7], "task-name", "0000000")
+			task := &drivers.TaskConfig{
+				ID:        taskID,
+				AllocID:   allocID,
+				Resources: createBasicResources(),
+			}
+
+			must.NoError(t, task.EncodeConcreteDriverConfig(&taskCfg))
+
+			mv := &mockVirtualizar{
+				count: 0,
+				err:   tt.virtErr,
+			}
+
+			mockTaskGetter := &mockTaskGetter{
+
+				info: &domain.Info{
+					State: libvirt.DomainRunning,
+				},
+			}
+
+			mockImageHandler := &mockImageHandler{
+				imageFormat: "tif",
+			}
+
+			d := virtDriverHarness(t, mv, nil, mockTaskGetter, mockImageHandler, tempDir)
+			cleanup := d.MkAllocDir(task, true)
+			defer cleanup()
+
+			_, _, err := d.StartTask(task)
+			must.Eq(t, nil, errors.Unwrap(err))
+
+		})
 	}
 }
