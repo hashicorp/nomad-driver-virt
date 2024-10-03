3933e8d53a4e8e0169a091474f494f0e85f8281e
James Rasell <jrasell@hashicorp.com>
2024-10-03 08:23:09 +0100

    net: correctly return and handle started response with no network.
    It is possible operators do not configure a network for a VM
    driver task. In this case, the plugin should correctly handle the
    response from the network controller.
    
    Along with the main fix, the change updates the controller
    responses, so that an object is returned when there isn't an
    error. This is because returning nil,nil is a weird pattern in Go
    and catches people out; even the person that wrote it.

--- libvirt/net/net_default.go
+++ libvirt/net/net_default.go
@@ -17,11 +17,11 @@ func (c *Controller) Fingerprint(_ map[string]*structs.Attribute) {}
 func (c *Controller) Init() error { return nil }
 
 func (c *Controller) VMStartedBuild(_ *net.VMStartedBuildRequest) (*net.VMStartedBuildResponse, error) {
-	return nil, nil
+	return &net.VMStartedBuildResponse{}, nil
 }
 
 func (c *Controller) VMTerminatedTeardown(_ *net.VMTerminatedTeardownRequest) (*net.VMTerminatedTeardownResponse, error) {
-	return nil, nil
+	return &net.VMTerminatedTeardownResponse{}, nil
 }
 
 func getInterfaceByIP(_ stdnet.IP) (string, error) { return "", nil }
--- libvirt/net/net_default_test.go
+++ libvirt/net/net_default_test.go
@@ -26,18 +26,19 @@ func TestController_Init(t *testing.T) {
 	must.NoError(t, mockController.Init())
 }
 
-func TestController_PostStart(t *testing.T) {
+func TestController_VMStartedBuild(t *testing.T) {
 	mockController := NewController(hclog.NewNullLogger(), &libvirt.ConnectMock{})
 	resp, err := mockController.VMStartedBuild(nil)
 	must.NoError(t, err)
-	must.Nil(t, resp)
+	must.NotNil(t, resp)
+	must.Nil(t, resp.TeardownSpec)
 }
 
-func TestController_PostStop(t *testing.T) {
+func TestController_VMTerminatedTeardown(t *testing.T) {
 	mockController := NewController(hclog.NewNullLogger(), &libvirt.ConnectMock{})
 	resp, err := mockController.VMTerminatedTeardown(nil)
 	must.NoError(t, err)
-	must.Nil(t, resp)
+	must.NotNil(t, resp)
 }
 
 func Test_getInterfaceByIP(t *testing.T) {
