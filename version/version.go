8f785e62979b5e0775001fdec3fb0e918338a208
Chris Roberts <croberts@hashicorp.com>
2026-05-19 10:31:15 -0700

    Release v0.0.2-beta.1

--- version/version.go
+++ version/version.go
@@ -11,12 +11,12 @@ var (
 
 	// Version is the main version number that is being run at the moment. It
 	// must conform to the format expected by github.com/hashicorp/go-version.
-	Version = "0.0.1"
+	Version = "0.0.2"
 
 	// VersionPrerelease is a pre-release marker for the version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta.1", "rc1.1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = "beta.1"
 
 	// VersionMetadata is metadata further describing the build type.
 	VersionMetadata = ""
