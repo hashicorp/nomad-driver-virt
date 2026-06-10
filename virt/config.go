7eb90ea156d44ac08f25fa143c4a80b064db3e1b
Chris Roberts <croberts@hashicorp.com>
2026-06-10 07:53:09 -0700

    config: remove deprecated configuration (#231)
    Removes the deprecated configuration options and the compat helpers
    to move the configuration values into the correct locations.

--- virt/config.go
+++ virt/config.go
@@ -5,7 +5,6 @@ package virt
 
 import (
 	"fmt"
-	"path/filepath"
 	"slices"
 	"strings"
 	"time"
@@ -19,27 +18,11 @@ import (
 	"github.com/hashicorp/nomad/plugins/shared/hclspec"
 )
 
-const (
-	// These were the original default path and pool name used within
-	// the libvirt provider.
-	compatDefaultStoragePath = "/var/lib"
-	compatDefaultStoragePool = "virt-sp"
-)
-
 var (
 	configSpec = hclspec.NewObject(map[string]*hclspec.Spec{
-		"emulator": hclspec.NewBlock("emulator", false, hclspec.NewObject(map[string]*hclspec.Spec{
-			"uri": hclspec.NewDefault(
-				hclspec.NewAttr("uri", "string", false),
-				hclspec.NewLiteral(`"qemu:///system"`),
-			),
-			"user":     hclspec.NewAttr("user", "string", false),
-			"password": hclspec.NewAttr("password", "string", false),
-		})),
 		"provider": hclspec.NewBlock("provider", false, hclspec.NewObject(map[string]*hclspec.Spec{
 			"libvirt": libvirt.ConfigSpec(),
 		})),
-		"data_dir":      hclspec.NewAttr("data_dir", "string", false),
 		"image_paths":   hclspec.NewAttr("image_paths", "list(string)", false),
 		"storage_pools": hclspec.NewBlock("storage_pools", false, storage.ConfigSpec()),
 	})
@@ -51,9 +34,6 @@ var (
 	taskConfigSpec = hclspec.NewObject(map[string]*hclspec.Spec{
 		"network_interface":               net.NetworkInterfaceHCLSpec(),
 		"disk":                            disks.ConfigSpec(),
-		"use_thin_copy":                   hclspec.NewAttr("use_thin_copy", "bool", false),
-		"primary_disk_size":               hclspec.NewAttr("primary_disk_size", "number", false),
-		"image":                           hclspec.NewAttr("image", "string", false),
 		"hostname":                        hclspec.NewAttr("hostname", "string", false),
 		"user_data":                       hclspec.NewAttr("user_data", "string", false),
 		"default_user_authorized_ssh_key": hclspec.NewAttr("default_user_authorized_ssh_key", "string", false),
@@ -82,7 +62,6 @@ func TaskConfigSpec() *hclspec.Spec {
 // TaskConfig contains configuration information for a task that runs within
 // this plugin.
 type TaskConfig struct {
-	ImagePath           string         `codec:"image"`
 	Hostname            string         `codec:"hostname"`
 	OS                  *OS            `codec:"os"`
 	UserData            string         `codec:"user_data"`
@@ -90,8 +69,6 @@ type TaskConfig struct {
 	CMDs                []string       `codec:"cmds"`
 	DefaultUserSSHKey   string         `codec:"default_user_authorized_ssh_key"`
 	DefaultUserPassword string         `codec:"default_user_password"`
-	UseThinCopy         bool           `codec:"use_thin_copy"`
-	PrimaryDiskSize     uint64         `codec:"primary_disk_size"`
 	Disks               disks.Disks    `codec:"disk"`
 	// The list of network interfaces that should be added to the VM.
 	net.NetworkInterfacesConfig `codec:"network_interface"`
@@ -102,17 +79,9 @@ type OS struct {
 	Machine string `codec:"machine"`
 }
 
-type Emulator struct {
-	URI      string `codec:"uri"`
-	User     string `codec:"user"`
-	Password string `codec:"password"`
-}
-
 // Config contains configuration information for the plugin
 type Config struct {
-	Emulator     *Emulator       `codec:"emulator"`
 	Provider     *Provider       `codec:"provider"`
-	DataDir      string          `codec:"data_dir"`
 	ImagePaths   []string        `codec:"image_paths"` // allow-list of host paths to load
 	StoragePools *storage.Config `codec:"storage_pools"`
 }
@@ -132,52 +101,6 @@ func (c *Config) Validate() error {
 	)
 
 	return mErr.ErrorOrNil()
-}
-
-// Compat sets appropriate configuration using deprecated options.
-func (c *Config) Compat() {
-	if c.Emulator != nil {
-		if c.Provider == nil {
-			c.Provider = &Provider{Libvirt: &libvirt.Config{}}
-		}
-
-		if c.Provider.Libvirt == nil {
-			c.Provider.Libvirt = &libvirt.Config{}
-		}
-
-		if c.Provider.Libvirt.URI == "" {
-			c.Provider.Libvirt.URI = c.Emulator.URI
-		}
-
-		if c.Provider.Libvirt.User == "" {
-			c.Provider.Libvirt.User = c.Emulator.User
-		}
-
-		if c.Provider.Libvirt.Password == "" {
-			c.Provider.Libvirt.Password = c.Emulator.Password
-		}
-	}
-
-	// If the deprecated DataDir value is set, use it to create the compat pool.
-	if c.DataDir != "" {
-		if c.StoragePools == nil {
-			c.StoragePools = storage.NewConfig()
-		}
-
-		c.StoragePools.Directory[compatDefaultStoragePool] = storage.Directory{
-			Path: filepath.Join(c.DataDir, compatDefaultStoragePool),
-		}
-	}
-
-	// If no storage pools are defined, add in the compat pool.
-	if c.StoragePools == nil || (len(c.StoragePools.Ceph) == 0 && len(c.StoragePools.Directory) == 0) {
-		if c.StoragePools == nil {
-			c.StoragePools = storage.NewConfig()
-		}
-		c.StoragePools.Directory[compatDefaultStoragePool] = storage.Directory{
-			Path: filepath.Join(compatDefaultStoragePath, compatDefaultStoragePool),
-		}
-	}
 }
 
 // Provider contains provider specific configuration
