d90dfba9edd49d52b27a9d3a3df7b55eeda1edf7
Chris Roberts <croberts@hashicorp.com>
2026-09-16 14:21:26 -0700

    filter: add shared linux types (#255)
    Adds generic types for chains and rules along with a request type
    for collecting chains and rules to be added to the packet filter.
    A teardown type is also included for removing chains and rules from
    the packet filter.

---
+++ net/filter/linux/shared/spec.go
@@ -0,0 +1,28 @@
+// Copyright IBM Corp. 2024, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+//go:build linux
+
+package shared
+
+import "fmt"
+
+// Spec represents the specification of a rule.
+type Spec struct {
+	Destination     string
+	DestinationPort int
+	DestinationType string
+	InInterface     string
+	Jump            string
+	OutInterface    string
+	Protocol        string
+	Source          string
+	SourceType      string
+	State           string
+	ToDestination   string
+}
+
+// Hash returns a unique string for the spec.
+func (s Spec) Hash() string {
+	return fmt.Sprintf("%#v", s)
+}
